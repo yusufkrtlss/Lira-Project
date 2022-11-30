5b71f2a2910d933310ca919e6ebc5017519f8d03
yusufkrtlss <krtlss.yusuf@gmail.com>
2022-11-30 17:27:00 +0300

    CustomerMap

---
+++ LiraProject/DataAccessLayer/Concrete/EntityFramework/Mapping/CustomerMap.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EntityFramework.Mapping
+{
+    public class CustomerMap : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
